224ec8323c4bd22139afa24d6bfc86b9b87e5c70
TIC <TIC@PCFORMACION015.ayesa.es>
2017-10-26 20:07:51 +0200

    prueba

--- karaofix/src/main/java/com/mycompany/karaofix/Fixer.java
+++ karaofix/src/main/java/com/mycompany/karaofix/Fixer.java
@@ -11,6 +11,24 @@ package com.mycompany.karaofix;
  */
 public class Fixer {
     public static void main(String[] args) {
-        // TODO: Completar el Ejercicio aquí. :-D
+
+
+//		 Path path = Paths.get("C:\\Users\\TIC\\Desktop\\hola.txt");
+//		 Path copia = Paths.get("C:\\Users\\TIC\\Desktop\\copiaDeHola.txt");
+//		 Charset charset = Charset.forName("UTF-8");
+//		 String line = null;
+//		 try (BufferedReader reader = Files.newBufferedReader(path);
+//		 PrintWriter writer = new PrintWriter(Files.newBufferedWriter(copia))) {
+//		
+//		 while (line != null) {
+//		 System.out.println(line);
+//		 line = reader.readLine();
+//		 }
+//		 } catch (IOException x) {
+//		 System.out.println("Error al leer");
+//		 x.printStackTrace();
+//		 }
+    
+    
     }
 }
